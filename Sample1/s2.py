475602f8d146f0a977d619d6cf9ae8cf1a8cfef5
itsmanojreddyy <94695026+itsmanojreddyy@users.noreply.github.com>
2021-12-08 10:21:32 +0530

    Update s2.py

--- Sample1/s2.py
+++ Sample1/s2.py
@@ -6,6 +6,6 @@ class TestStringMethods(unittest.TestCase):
     def test_bDifferenceMean(self):
         print("Sample test")
         self.assertEqual("1","2","Values not as per expectation @@")
-    if __name__ == "__main__":
+if __name__ == "__main__":
     unittest.main()
 
